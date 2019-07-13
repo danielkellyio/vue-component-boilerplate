cf4ced7b82905b763b78d954856bffe7a080c50e
Daniel Kelly <danielkelly@randallreilly.com>
2019-07-13 09:53:23 -0500

    added comment to explain extractCSS

--- bili.config.js
+++ bili.config.js
@@ -2,6 +2,10 @@ module.exports = {
   presets: ['bili/babel'],
   output: {
     moduleName: 'optiImage',
+    /**
+     * false means styles are added via a style tag appended to the head (client side only)
+     * true means separate stylesheet is created
+     */
     extractCSS: false,
   },
   plugins: {
